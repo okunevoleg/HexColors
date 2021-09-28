a69e63b71d73fa41a1087aa74da2eb33f72a4ff4
Oleg <danilkurzik@gmail.com>
2021-09-28 23:10:52 +0600

    HexColor

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+
+    function changeColor() {
+        let hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+
+        let hexcode = '';
+
+        for (let i = 0; i < 6; i++) {
+            let randomIndex = Math.floor(Math.random() * hexNumbers.length);
+
+
+            hexcode += hexNumbers[randomIndex];
+        }
+
+        document.querySelector('#hex-code').innerHTML = hexcode;
+        document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+        
+    }
